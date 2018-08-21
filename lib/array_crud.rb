060847a456871668a88f9c951aa5a4f42b97f39a
Rob Parker <into.the.parker@gmail.com>
2018-08-21 14:56:04 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,29 +7,29 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  content_array.push('Apex')
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  content_array.unshift('Ant')
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  content_array.pop
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  content_array.shift
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  content_array[0]
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-
+ array[0]
 end
 
 def retrieve_last_element_from_array(array)
-
+ array[-1]
 end
